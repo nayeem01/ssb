ef950e3c497c172ae1cdcb9de094077d1fc9487e
nayeem01 <nayeemabdullah00@gmail.com>
2020-06-12 12:54:03 +0600

    vlog folder from admin LTE has been added. templete has been setup

---
+++ vlog/comment.php
@@ -0,0 +1,52 @@
+<?php include('inc/header.php'); ?>
+<?php include('inc/topbar.php'); ?>
+<?php include('inc/menu.php'); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">manage all comments</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Dashboard v1</li>
+                </ol>
+            </div><!-- /.col -->
+            </div><!-- /.row -->
+           </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+   <!-- Body content start -->
+   <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- card strat -->
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                             <h5 class="card-title">Card title</h5>
+
+                                <p class="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the card's
+                                content.
+                                </p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                     </div> 
+                        <!-- card end -->
+                </div>
+           </div>
+        </div>
+   </section>
+   <!-- Body content end -->
+
+
+</div>
+<?php include('inc/footer.php'); ?>
