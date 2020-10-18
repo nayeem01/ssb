e46b1c07f22c452cd45eb07df088b9b446a28b66
nayeem01 <nayeemabdullah00@gmail.com>
2020-10-18 10:45:18 +0600

    catagory created

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\PageController;
 use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\Backend\CategoryController;
 /*
 Route::get('/', function () {
     return view('backend.pages.index');
@@ -21,6 +22,15 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('/edit/{id}', [BrandController::class,'edit'])->name('brands.edit');
         Route::post('/edit/{id}', [BrandController::class,'update'])->name('brands.update');
         Route::post('/destroy/{id}', [BrandController::class,'destroy'])->name('brands.destroy');
+
+    });
+    Route::group(['prefix' => '/categorys'], function () {
+        Route::get('/manage', [CategoryController::class,'index'])->name('cat.manage');
+        Route::get('/create', [CategoryController::class,'create'])->name('cat.create');
+        Route::post('/store', [CategoryController::class,'store'])->name('cat.store');
+        Route::get('/edit/{id}', [CategoryController::class,'edit'])->name('cat.edit');
+        Route::post('/edit/{id}', [CategoryController::class,'update'])->name('cat.update');
+        Route::post('/destroy/{id}', [CategoryController::class,'destroy'])->name('cat.destroy');
 
     });
 });
